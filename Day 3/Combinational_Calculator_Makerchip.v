624ae8af72ae7ea00ebf2b17baca1351bd8314d7
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2025-04-06 11:26:27 +0530

    Update Combinational_Calculator_Makerchip.v

--- Day 3/Combinational_Calculator_Makerchip.v
+++ Day 3/Combinational_Calculator_Makerchip.v
@@ -1,9 +1,11 @@
+//=====Link of the project: www.makerchip.com/sandbox/0o2fXho92/0RghvAz====//
+
 \m5_TLV_version 1d: tl-x.org
 \m5
    
-   // =================================================
-   // =============Combinational Calculator============
-   // =================================================
+   // =================================================//
+   // =============Combinational Calculator============//
+   // =================================================//
    
    //use(m5-1.0)   /// uncomment to use M5 macro library.
 \SV
@@ -29,4 +31,3 @@
 \SV
    endmodule
 
-Link: www.makerchip.com/sandbox/0o2fXho92/0RghvAz
