19c1fc4127db4ff99c68918da93842762acf49c9
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2025-04-06 11:25:23 +0530

    Update Pythagoras Theorem Makerchip.v

--- Day 3/Pythagoras Theorem Makerchip.v
+++ Day 3/Pythagoras Theorem Makerchip.v
@@ -1,37 +1,32 @@
-//=========================================================================//
-//  ===============Pythagoras Theorem code in TL Verilog===================//
-//=========================================================================//
+//=====Link of the project: www.makerchip.com/sandbox/0o2fXho92/0X6hXxw====//
+
+//=================================================//
+//=============Pythogoras Theorem==================//
+//=================================================//
+
+
 \m4_TLV_version 1d: tl-x.org
 \SV
    `include "sqrt32.v";
    
-   m5_makerchip_module
+   m4_makerchip_module
+
 \TLV
    
    |calc
-      ?$valid
-         @1
-            $a[7:0] = $rand1[3:0];
-            $b[7:0] = $rand2[3:0];
-            $aa[15:0] = $aa[7:0] ** 2;
-            $bb[15:0] = $bb[7:0] ** 2;
-         @2
-            $cc[15:0] = $aa + $bb;
-         @3
-            $c[7:0] = sqrt($cc);
-
-
-   // Print
-   |calc
+      @1
+         $a[7:0] = $rand1[3:0];
+         $b[7:0] = $rand2[3:0];
+         $aa[15:0] = $aa[7:0] ** 2;
+         $bb[15:0] = $bb[7:0] ** 2;
+      @2
+         $cc[15:0] = $aa + $bb;
       @3
-         \SV_plus
-            always_ff @(posedge clk) begin
-               if ($valid)
-                  \$display("sqrt((\%2d ^ 2) + (\%2d ^ 2)) = \%2d", $aa, $bb, $cc);
-            end
+         $c[7:0] = sqrt($cc);
 
+   
+   // Assert these to end simulation (before Makerchip cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
 \SV
    endmodule
-
-
-Link of the project: www.makerchip.com/sandbox/0o2fXho92/0Wnh5A7
