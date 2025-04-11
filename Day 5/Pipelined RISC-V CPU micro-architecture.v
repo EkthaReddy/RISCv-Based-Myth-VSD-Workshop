89d0f3509306013a21e8dd4b7be6bc165edaf8b4
Ektha Reddy <152515939+EkthaReddy@users.noreply.github.com>
2025-04-11 11:54:23 +0530

    Update and rename Pipline RISCv Code.v to Pipelined RISC-V CPU micro-architecture.v

--- Day 5/Pipelined RISC-V CPU micro-architecture.v
+++ Day 5/Pipelined RISC-V CPU micro-architecture.v
@@ -32,6 +32,8 @@
    m4_asm(ADDI, r13, r13, 1)            // Increment intermediate register by 1
    m4_asm(BLT, r13, r12, 1111111111000) // If a3 is less than a2, branch to label named <loop>
    m4_asm(ADD, r10, r14, r0)            // Store final result to register a0 so that it can be read by main program
+   m4_asm(SW, r0, r10, 100)
+   m4_asm(LW, r15,r0, 100)
    
    // Optional:
    // m4_asm(JAL, r7, 00000000000000000000) // Done. Jump to itself (infinite loop). (Up to 20-bit signed immediate plus implicit 0 bit (unlike JALR) provides byte address; last immediate bit should also be 0)
@@ -44,7 +46,13 @@
          
          $imem_rd_en = ~ $reset;
          
-         $pc[31:0] = 
+         $pc[31:0] = >>1$reset ? 0 :
+                     >>3$valid_taken_br ? >>3$br_tgt_pc :
+                     >>3$valid_load ? >>3$pc :
+                     >>3$valid_jump && >>3$is_jal ?  >>3$br_tgt_pc :
+                     >>3$valid_jump && >>3$is_jalr ? >>3$jalr_tgt_pc : 
+                     >>1$pc + 32'd4;
+         
          //if reset is active assign to zero
          // Or else, the branch taken 1 cycle ago should be activated for respective branch instruction
          // Or else the pc must be incremented by 4 instruction
@@ -81,7 +89,7 @@
          
          $imm[31:0] = $is_i_instr ? { {21{$instr[31]}}, $instr[30:20] } :
                       $is_s_instr ? { {21{$instr[31]}}, $instr[30:25], $instr[11:8], $instr[7]}:
-                      $is_b_instr ? { {20{$instr[31]}}, $instr[7], $instr[30:25], $instr[11:18], 1'b0} :
+                      $is_b_instr ? { {20{$instr[31]}}, $instr[7], $instr[30:25], $instr[11:8], 1'b0} :
                       $is_u_instr ? { $instr[31], $instr[30:20], $instr[19:12], {12{1'b0}}} :
                       $is_j_instr ? { {12{$instr[31]}}, $instr[19:12], $instr[20], $instr[30:25], $instr[24:21], 1'b0}:
                       32'b0;
@@ -157,25 +165,45 @@
          $is_and = $dec_bits ==? 11'b0_111_0110011;
          
          $is_load = $dec_bits ==? 11'bx_xxx_0000011;
+         
          //Until the instrs are implemented, quiet down the warnings
          
          `BOGUS_USE($is_beq $is_bne $is_blt $is_bge $is_bltu $is_bgeu
-                    $is_auipc $is_jal $is_jalr $is_lb $is_lh $is_lw $is_lbu $is_lhu $is_sb $is_sh $is_sw 
+                    $is_lui $is_auipc $is_jal $is_jalr $is_lb $is_lh $is_lw $is_lbu $is_lhu $is_sb $is_sh $is_sw 
                     $is_addi $is_slti $is_sltiu $is_xori $is_ori $is_andi $is_slli $is_srli $is_srai
                     $is_add $is_sub $is_sll $is_slt $is_sltu $is_xor $is_srl $is_sra $is_or $is_and
                     $is_load);
       @2  
          //Read function of register file
          
-             
+         $rf_rd_en1 = $rs1_valid;
+         $rf_rd_index1[4:0] = $rs1[4:0];
          
+         $rf_rd_en2 = $rs2_valid;
+         $rf_rd_index2[4:0] = $rs2[4:0];
+         
+         
+         $src1_value[31:0] = ($rf_rd_index1 == >>1$rf_wr_index) ? 
+                    ( >>1$rf_wr_en ? >>1$result[31:0] : $rf_rd_data1[31:0] )
+                    : $rf_rd_data1[31:0];
+         
+         $src2_value[31:0] = ($rf_rd_index2 == >>1$rf_wr_index) ? 
+                    ( >>1$rf_wr_en ? >>1$result[31:0] : $rf_rd_data2[31:0] )
+                    : $rf_rd_data2[31:0];
+         //If the read index and write index are same 
+         //then the data that goes to mux1 can either be result of ALU or data 1 of read file
+         //If read and write are not same
+         //then directly the data1 goes to ALU src1
+         // Similary for the src2 value.
+         
+         $br_tgt_pc[31:0] = $pc[31:0] +$imm[31:0];           
       @3   
          //ALU
          $result[31:0] = $is_addi ? $src1_value + $imm:
                          $is_add ? $src1_value + $src2_value:
                          $is_sub ? $src1_value - $src2_value:
                          $is_sll ? $src1_value << $src2_value[4:0]:
-                         $is_srl $src1_value >> $src2_value[4:0]:
+                         $is_srl ? $src1_value >> $src2_value[4:0]:
                          $is_sltu ? $src1_value < $src2_value:
                          $is_sltiu ? $src1_value <$imm:
                          
@@ -184,7 +212,7 @@
                          $is_xori ? $src1_value ^ $imm:
                          $is_slli ? $src1_value << $imm[5:0]:
                          $is_srli ? $src1_value >> $imm[5:0]:
-                         $is_and ? $src1_value $src2_value:
+                         $is_and ? $src1_value & $src2_value:
                          $is_or ? $src1_value | $src2_value:
                          $is_xor ? $src1_value ^ $src2_value:
                          
@@ -197,16 +225,15 @@
                          
                          $is_srai ? { {32{$src1_value[31]}}, $src1_value} >> $imm[4:0]:
                          $is_slt ? ($src1_value[31] == $src2_value[31]) ? ($src1_value < $src2_value) : {31'b0, $src1_value[31]}:
-                         $is_slti ? (src1_value[31] == $imm[31]) ? ($src1_value ^ $src2_value) : {31'b0, $src1_value[31]}:
+                         $is_slti ? ($src1_value[31] == $imm[31]) ? ($src1_value ^ $src2_value) : {31'b0, $src1_value[31]}:
                          $is_sra ? { {32{$src1_value[31]}}, $src1_value} >> $src2_value[4:0]:
                          32'bx;
-               
+           
+         
          //Write Register File      
-
-         
-         
-         //Valid 
-         
+         $rf_wr_en = ($rd && $rd_valid && $valid) || >>2$valid_load;
+         $rf_wr_index[4:0] = >>2$valid_load ? >>2$rd[4:0] : $rd[4:0];
+         $rf_wr_data[31:0] = >>2$valid_load ? >>2$ld_data[31:0]: $result[31:0];
          
          // Branch Instructions
          $taken_br = $is_b_instr ?
@@ -218,16 +245,32 @@
                      $is_bgeu ? ($src1_value >= $src2_value): 1'b0):
                      1'b0;
          
-         // Jump target 
          
+         //Valid Instructions 
+         $valid_load = $valid && $is_load;
+         
+         $valid_jump = $valid && ($is_jal || $is_jalr );
+         
+         $valid_taken_br = $taken_br && $valid;
+         
+         $valid = !(>>1$taken_br || >>2$taken_br || 
+                   >>1$valid_load || >>2$valid_load || 
+                   >>1$valid_jump || >>2$valid_jump);
+                   
+         // Jump target 
+         $jalr_tgt_pc[31:0] = $src1_value[31:0] + $imm[31:0];
          
       @4   
-         //Load Data
+         //Data Memory
+         $dmem_wr_en = $is_s_instr && $valid;
+         $dmem_rd_en = $is_load;
+         $dmem_addr[3:0] = $result[5:2];
+         $dmem_wr_data[31:0] = $src2_value;
          
          
          
       @5
-         
+         $ld_data[31:0] = $dmem_rd_data[31:0];
          
    // Assert these to end simulation (before Makerchip cycle limit).
    *passed = |cpu/xreg[10]>>5$value == (1+2+3+4+5+6+7+8+9);
